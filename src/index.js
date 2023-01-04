7ff2e7b2713dc8cf6499d9821399b8de93f94470
Roman Pavlenko <roman.pavlenko@qbex.io>
2023-01-04 12:24:52 +0200

    adaptive to services block

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-const brandsGallery = document.getElementById('brandsGallery');
-const brandsSlide = document.getElementById('brandsSlide');
-const slides = brandsSlide.getElementsByClassName('brands__logo');
-const slideWidth = document.getElementsByClassName('brands__logo')[0].offsetWidth;
+function brandsSlider () {
+  const brandsGallery = document.getElementById('brandsGallery');
+  const brandsSlide = document.getElementById('brandsSlide');
+  const slides = brandsSlide.getElementsByClassName('brands__logo');
+  const slideWidth = document.getElementsByClassName('brands__logo')[0].offsetWidth;
 
-for (let i = 0; i < slides.length; i++) {
-  slides[i].style.left = i * slideWidth + 'px';
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.left = i * slideWidth + 'px';
+  }
+
+  setInterval(() => {
+    for (let i = 0; i < slides.length; i++) {
+      const styleOfFirstElement = parseInt(slides[i].style.left);
+      const isLessThenZero = (styleOfFirstElement < 0);
+      slides[i].style.left = styleOfFirstElement - slideWidth + 'px';
+
+      if(isLessThenZero) {
+        slides[i].style.visibility = 'hidden';
+        slides[i].style.left = ((slides.length - 2) * slideWidth) + 'px';
+      } else if (slides[i].style.visibility !== 'visible'){
+        slides[i].style.visibility = 'visible';
+      }
+    }
+  }, 3000);
 }
 
-setInterval(() => {
-  for (let i = 0; i < slides.length; i++) {
-    const lefty = parseInt(slides[i].style.left);
-    const isLeftZero = (lefty < 0);
-    slides[i].style.left = lefty - slideWidth + 'px';
-
-    if(isLeftZero) {
-      slides[i].style.visibility = 'hidden';
-      slides[i].style.left = ((slides.length - 2) * slideWidth) + 'px';
-    } else {
-      slides[i].style.visibility = 'visible';
-    }
-  }
-}, 3000);
+document.addEventListener('load', brandsSlider());
